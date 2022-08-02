2ee1be5f432823899e9439abc0a63a252205d4e9
Miriam2106 <20203tn055@utez.edu.mx>
2022-08-02 08:55:15 -0500

    Responder encuesta

--- js/control/permanencia/estadia/ajsControlEncuestas.js
+++ js/control/permanencia/estadia/ajsControlEncuestas.js
@@ -3,9 +3,11 @@ var sisa = angular.module("sisa", ['ui.bootstrap', 'oitozero.ngSweetAlert']);
 sisa.controller("ControlEncuesta", ['$rootScope', '$scope', '$http', 'SweetAlert', function ($rootScope, $scope, $http, SweetAlert) {
     $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
     $scope.encuesta = [];
+    $scope.arregloRespuestas = [];
     $scope.radio1 = false;
     $scope.valor = "";
     $scope.verEncuesta = false
+    $scope.load = false
     $scope.preguntaPrueba = {
         respuesta: ""
     }
@@ -21,10 +23,6 @@ sisa.controller("ControlEncuesta", ['$rootScope', '$scope', '$http', 'SweetAlert
         ;
     });
 
-    $scope.cambiarEstado = function () {
-        $scope.radio1 = !$scope.radio1;
-    }
-
     $scope.consultarEncuesta = function () {
         let data = {
             nombre: "Satisfacción de Estadías"
@@ -34,53 +32,6 @@ sisa.controller("ControlEncuesta", ['$rootScope', '$scope', '$http', 'SweetAlert
         $scope.encuesta.carreraPerfil = "Desarrollo De Software Multiplataforma"
         $scope.encuesta.grupo = "6B"
     };
-
-    $scope.classActive = function (clase) {
-        console.log(clase)
-        let head = document.getElementById(`head${clase}`)
-        let redirect = document.getElementById(`redirect${clase}`)
-        let headRemovido = document.getElementById(`head${clase-1}`)
-
-        head.className="ng-scope active"
-        redirect.style.cssText = "pointer-events: all;"
-        headRemovido.className="ng-scope"
-
-    };
-
-    $scope.classFinalizar = function (clase) {
-        let head = document.getElementById("headFinalizar")
-        let redirect = document.getElementById("redirectFinalizar")
-        let headRemovido = document.getElementById(`head${clase}`)
-
-        head.className="active"
-        redirect.style.cssText = "pointer-events: all;"
-        headRemovido.className="ng-scope"
-    };
-
-    $scope.comenzarEncuesta = function () {
-        SweetAlert.swal({
-            title: "¿Estas seguro(a) de comenzar la encuesta?",
-            text: "Recuerda que sólo puedes responderla una sola vez y tus respuestas no se podrán modificar.",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Aceptar",
-            cancelButtonText: "Cancelar",
-            closeOnConfirm: true,
-            closeOnCancel: true,
-        }, function (isConfirm) {
-            if (isConfirm) {
-                $scope.verEncuesta = true
-                $scope.mostrarEncuesta()
-            } else {
-                console.log("okis")
-            }
-        });
-    }
-
-    $scope.change = function () {
-        console.log($scope.sino);
-    }
 
     $scope.mostrarEncuesta = function () {
         let encuesta = {
@@ -97,24 +48,28 @@ sisa.controller("ControlEncuesta", ['$rootScope', '$scope', '$http', 'SweetAlert
                             enunciado: "¿La información que se te proporcionó en las guías de estadía, te pareció?",
                             indice: 2,
                             tipos_respuestas: "SiNo",
+                            obligatoria: 0
                         },
                         {
                             id: 2,
                             enunciado: "¿La información que te proporcionó el departamento durante el curso de inducción a la estadía, te pareció?",
                             indice: 1,
-                            tipos_respuestas: "Escala"
+                            tipos_respuestas: "Escala",
+                            obligatoria: 1
                         },
                         {
                             id: 3,
                             enunciado: "Agrega cualquier comentario que quieras hacer respecto al departamento de estadías:",
                             indice: 3,
-                            tipos_respuestas: "Libre"
+                            tipos_respuestas: "Libre",
+                            obligatoria: 1
                         },
                         {
                             id: 4,
                             enunciado: "Selecciona las maneras en que recibiste apoyo ",
                             indice: 4,
                             tipos_respuestas: "Multiple",
+                            obligatoria: 1,
                             respuestas: [
                                 {
                                     id: 1,
@@ -139,6 +94,7 @@ sisa.controller("ControlEncuesta", ['$rootScope', '$scope', '$http', 'SweetAlert
                             enunciado: "¿Recibiste algún apoyo por parte de la empresa?",
                             indice: 1,
                             tipos_respuestas: "SiNo",
+                            obligatoria: 1,
                             subpreguntas: [
                                 {
                                     id: 1,
@@ -170,13 +126,15 @@ sisa.controller("ControlEncuesta", ['$rootScope', '$scope', '$http', 'SweetAlert
                             id: 2,
                             enunciado: "¿El apoyo que te brindó la empresa o institución respecto a las herramientas necesarias (equipo de oficina, seguridad, herramientas, material, etc.) para realizar tus actividades durante el periodo de estadía te pareció?",
                             indice: 2,
-                            tipos_respuestas: "Escala"
+                            tipos_respuestas: "Escala",
+                            obligatoria: 1
                         },
                         {
                             id: 3,
                             enunciado: " ¿El tiempo estimado que le dedicaste a tu proyecto durante la estadía en la empresa o institución, te pareció?",
                             indice: 3,
-                            tipos_respuestas: "Escala"
+                            tipos_respuestas: "Escala",
+                            obligatoria: 1
                         },
                     ]
                 },
@@ -190,25 +148,29 @@ sisa.controller("ControlEncuesta", ['$rootScope', '$scope', '$http', 'SweetAlert
                             id: 1,
                             enunciado: "¿En general la atención y/o orientación de tu asesora / asesor  universitario, te pareció?",
                             indice: 1,
-                            tipos_respuestas: "Escala"
+                            tipos_respuestas: "Escala",
+                            obligatoria: 1
                         },
                         {
                             id: 2,
                             enunciado: "¿Tu asesora / asesor universitario te informó y orientó acerca del proyecto, los formatos y trámites que debías seguir?",
                             indice: 3,
-                            tipos_respuestas: "SiNo"
+                            tipos_respuestas: "SiNo",
+                            obligatoria: 1
                         },
                         {
                             id: 3,
                             enunciado: "¿Tu asesora o asesor universitario te presentó en la empresa o institución al inicio de tu estadía (vía correo electrónico)?",
                             indice: 2,
-                            tipos_respuestas: "SiNo"
+                            tipos_respuestas: "SiNo",
+                            obligatoria: 1
                         },
                         {
                             id: 4,
                             enunciado: "Especifica el número total visitas que la asesora / asesor universitario te realizó en la empresa o institución:",
                             indice: 4,
                             tipos_respuestas: "Select",
+                            obligatoria: 1,
                             respuestas: [
                                 {
                                     id: 1,
@@ -233,6 +195,75 @@ sisa.controller("ControlEncuesta", ['$rootScope', '$scope', '$http', 'SweetAlert
             ]
         }
         $scope.encuesta = encuesta;
+            $scope.load = false
+    }
+
+    $scope.comenzarEncuesta = async()  => {
+        SweetAlert.swal({
+            title: "¿Estas seguro(a) de comenzar la encuesta?",
+            text: "Recuerda que sólo puedes responderla una sola vez y tus respuestas no se podrán modificar.",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Aceptar",
+            cancelButtonText: "Cancelar",
+            closeOnConfirm: true,
+            closeOnCancel: true,
+        }, function (isConfirm) {
+            if (isConfirm) {
+                console.log("ya")
+                $scope.verEncuesta = true
+                $scope.mostrarEncuesta()
+                console.log($scope.verEncuesta)
+                console.log($scope.load)
+            } else {
+                console.log("okis")
+            }
+        });
+    }
+    
+    $scope.siguiente = function (clase) {
+        console.log(clase)
+        let head = document.getElementById(`head${clase+1}`)
+        let headRemovido = document.getElementById(`head${clase}`)
+
+        head.className="ng-scope active"
+        headRemovido.className="ng-scope"
+    };
+
+    $scope.anterior = function (clase) {
+        console.log(clase)
+        let head = document.getElementById(`head${clase-1}`)
+        let headRemovido = document.getElementById(`head${clase}`)
+
+        head.className="ng-scope active"
+        headRemovido.className="ng-scope"
+    };
+
+    $scope.cancelar = function (clase) {
+        console.log(clase)
+        let head = document.getElementById(`head${clase}`)
+        let headRemovido = document.getElementById("headFinalizar")
+
+        head.className="ng-scope active"
+        headRemovido.className="ng-scope"
+    };
+
+    $scope.classFinalizar = function (clase) {
+        let head = document.getElementById("headFinalizar")
+        let headRemovido = document.getElementById(`head${clase}`)
+
+        head.className="active"
+        headRemovido.className="ng-scope"
+    };
+
+    $scope.guardarRespuestas = function (value) {
+        console.log(value);
+    }
+
+    $scope.finalizar = () => {
+        console.log("Enviar respuestas")
+        console.log("a")
     }
 
 }]);
